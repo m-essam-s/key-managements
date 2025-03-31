952bfe5a7d4ed434ce6f64562d462c2f9c5f6cbb
m-essam-s <sir.m.essam.g@gmail.com>
2025-03-31 18:41:16 +0200

    add Home Route

---
+++ src/routes/Home.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+        <div className="flex flex-col items-center justify-center h-full bg-gray-100 p-4">
+            <h1 className="text-2xl font-bold mb-4">Home</h1>
+            <p className="mb-2">This is the home page of the Key Management System.</p>
+            <p className="mb-2">You can navigate to different sections using the links in the navbar.</p>
+        </div>
+    );
+}
+
+export default Home;
